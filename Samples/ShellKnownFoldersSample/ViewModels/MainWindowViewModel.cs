e145383c674534dc7cbe5bd2a9eab4c612eed3d5
starshipxac <starship.develop@starshipxac.net>
2015-02-13 23:12:19 +0900

    Add ShellKnownFoldersSample project.

---
+++ Samples/ShellKnownFoldersSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using Codeplex.Reactive;
+using Livet;
+using starshipxac.Shell;
+
+namespace ShellKnownFoldersSample.ViewModels
+{
+	public class MainWindowViewModel : ViewModel
+	{
+		public MainWindowViewModel()
+		{
+			#region Reactive Property
+
+			this.KnownFolders = new ReactiveCollection<ShellKnownFolderViewModel>(
+				ShellKnownFolders.AllFolders.Select(x => new ShellKnownFolderViewModel(x)).ToObservable());
+
+			#endregion
+		}
+
+		public void Initialize()
+		{
+		}
+
+		public ReactiveCollection<ShellKnownFolderViewModel> KnownFolders { get; private set; }
+	}
+}
