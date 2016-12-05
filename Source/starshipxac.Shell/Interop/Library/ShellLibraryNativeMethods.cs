1c6141fa68aa90078511fa8e5fcde5fd06dcb4ff
starshipxac <starshipxac@users.noreply.github.com>
2016-12-05 23:48:27 +0900

    SHCreateLibraryメソッド追加。

--- Source/starshipxac.Shell/Interop/Library/ShellLibraryNativeMethods.cs
+++ Source/starshipxac.Shell/Interop/Library/ShellLibraryNativeMethods.cs
@@ -5,12 +5,39 @@ namespace starshipxac.Shell.Interop.Library
 {
     internal static class ShellLibraryNativeMethods
     {
+        /// <summary>
+        ///     Creates an <see cref="IShellLibrary"/> object.
+        /// </summary>
+        /// <param name="riid"></param>
+        /// <param name="ppvLibrary"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///     https://msdn.microsoft.com/en-us/library/windows/desktop/dd378433(v=vs.85).aspx
+        /// </remarks>
+        [DllImport("Shell32", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        internal static extern HRESULT SHCreateLibrary(
+            [In] [MarshalAs(UnmanagedType.LPStruct)] Guid riid,
+            [Out] [MarshalAs(UnmanagedType.Interface)] out IShellLibrary ppvLibrary);
+
+        /// <summary>
+        ///     Shows the library management dialog box,
+        ///     which enables users to manage the library folders and default save location.
+        /// </summary>
+        /// <param name="psiLibrary"></param>
+        /// <param name="hwndOwner"></param>
+        /// <param name="pszTitle"></param>
+        /// <param name="pszInstruction"></param>
+        /// <param name="lmdOptions"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///     https://msdn.microsoft.com/en-us/library/windows/desktop/dd378441(v=vs.85).aspx
+        /// </remarks>
         [DllImport("Shell32", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Winapi, SetLastError = true)]
         internal static extern HRESULT SHShowManageLibraryUI(
-            [In] [MarshalAs(UnmanagedType.Interface)] IShellItem library,
+            [In] [MarshalAs(UnmanagedType.Interface)] IShellItem psiLibrary,
             [In] IntPtr hwndOwner,
-            [In] string title,
-            [In] string instruction,
+            [In] string pszTitle,
+            [In] string pszInstruction,
             [In] LIBRARYMANAGEDIALOGOPTIONS lmdOptions);
     }
 }
