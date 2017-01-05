856d259d3790a9ff3b58ba00da8bf6187bcbd0f0
starshipxac <starshipxac@users.noreply.github.com>
2017-01-05 22:39:52 +0900

    TaskDialogSample更新。

---
+++ Samples/TaskDialogSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using starshipxac.MinimalMVVM;
+
+namespace TaskDialogSample.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        public MainViewModel()
+        {
+            #region ReactiveProperty
+
+            this.SimpleTaskDialogCommand = new AsyncReactiveCommand();
+            this.SimpleTaskDialogCommand
+                .Subscribe(async _ => await this.View.ShowSimpleTaskDialogAsync())
+                .AddTo(this.CompositeDisposable);
+
+            this.AllControlsTaskDialogCommand = new AsyncReactiveCommand();
+            this.AllControlsTaskDialogCommand
+                .Subscribe(async _ => await this.View.ShowAllControlsTaskDialogAsync())
+                .AddTo(this.CompositeDisposable);
+
+            this.CustomButtonTaskDialogCommand = new AsyncReactiveCommand();
+            this.CustomButtonTaskDialogCommand
+                .Subscribe(async _ => await this.View.ShowCustomButtonTaskDialogAsync())
+                .AddTo(this.CompositeDisposable);
+
+            this.CommonLinkTaskDialogCommand = new AsyncReactiveCommand();
+            this.CommonLinkTaskDialogCommand
+                .Subscribe(async _ => await this.View.ShowCommandLinkTaskDialogAsync())
+                .AddTo(this.CompositeDisposable);
+
+            this.RadioButtonTaskDialogCommand = new AsyncReactiveCommand();
+            this.RadioButtonTaskDialogCommand
+                .Subscribe(async _ => await this.View.ShowRadioButtonTaskDialogAsync())
+                .AddTo(this.CompositeDisposable);
+
+            this.MarqueeTaskDialogCommand = new AsyncReactiveCommand();
+            this.MarqueeTaskDialogCommand
+                .Subscribe(async _ => await this.View.ShowMarqueeTaskDialogAsync())
+                .AddTo(this.CompositeDisposable);
+
+            #endregion
+        }
+
+        public void Loaded(dynamic view)
+        {
+            this.View = view;
+        }
+
+        public dynamic View { get; private set; }
+
+        public AsyncReactiveCommand SimpleTaskDialogCommand { get; }
+
+        public AsyncReactiveCommand AllControlsTaskDialogCommand { get; }
+
+        public AsyncReactiveCommand CustomButtonTaskDialogCommand { get; }
+
+        public AsyncReactiveCommand CommonLinkTaskDialogCommand { get; }
+
+        public AsyncReactiveCommand RadioButtonTaskDialogCommand { get; }
+
+        public AsyncReactiveCommand MarqueeTaskDialogCommand { get; }
+    }
+}
--- Samples/TaskDialogSample/Views/MainWindow.xaml.cs
+++ Samples/TaskDialogSample/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Threading;
 using starshipxac.Windows.Dialogs;
 using starshipxac.Windows.Dialogs.Controls;
 
@@ -14,176 +15,187 @@ namespace TaskDialogSample.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            if (this.DynamicDataContext != null)
-            {
-                this.DynamicDataContext.Loading(this);
-            }
         }
 
-        private dynamic DynamicDataContext => this.DataContext;
-
-        public void ShowSimpleTaskDialog()
+        public DispatcherOperation ShowSimpleTaskDialogAsync()
         {
-            using (var taskDialog = new TaskDialog())
+            return this.Dispatcher.InvokeAsync(() =>
             {
-                taskDialog.Title = "Simple Sample";
-                taskDialog.MainInstructionText = "Common Buttons";
-                taskDialog.CommonButtons =
-                    TaskDialogCommonButtons.Ok |
-                    TaskDialogCommonButtons.Yes |
-                    TaskDialogCommonButtons.No |
-                    TaskDialogCommonButtons.Cancel |
-                    TaskDialogCommonButtons.Retry |
-                    TaskDialogCommonButtons.Close;
+                using (var taskDialog = new TaskDialog())
+                {
+                    taskDialog.Title = "Simple Sample";
+                    taskDialog.MainInstructionText = "Common Buttons";
+                    taskDialog.CommonButtons =
+                        TaskDialogCommonButtons.Ok |
+                        TaskDialogCommonButtons.Yes |
+                        TaskDialogCommonButtons.No |
+                        TaskDialogCommonButtons.Cancel |
+                        TaskDialogCommonButtons.Retry |
+                        TaskDialogCommonButtons.Close;
 
-                var result = taskDialog.Show();
+                    var result = taskDialog.Show();
 
-                Debug.WriteLine("SelectedButton = " + result.SelectedButton);
-                Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
-                Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
-            }
+                    Debug.WriteLine("SelectedButton = " + result.SelectedButton);
+                    Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
+                    Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
+                }
+            });
         }
 
-        public void ShowAllControlsTaskDialog()
+        public DispatcherOperation ShowAllControlsTaskDialogAsync()
         {
-            using (var taskDialog = new TaskDialog())
+            return this.Dispatcher.InvokeAsync(() =>
             {
-                var minimum = 0;
-                var maximum = 10000;
-
-                taskDialog.Cancelable = true;
-                taskDialog.Title = "All Controls Sample";
-                taskDialog.MainInstructionText = "Main Instruction Text.";
-                taskDialog.ContentText = "Content Text.";
-                taskDialog.FooterText = "Footer Text.";
-                taskDialog.ExpandedText = "Expanded Text.";
-                taskDialog.ExpandedControlText = "Expanded Label";
-                taskDialog.CollapsedControlText = "Collapsed Label";
-                taskDialog.VerificationText = "Verification Text.";
-                taskDialog.MainIcon = TaskDialogIcon.Information;
-                taskDialog.FooterIcon = TaskDialogIcon.Warning;
-                taskDialog.ProgressBar = new TaskDialogProgressBar("progress")
+                using (var taskDialog = new TaskDialog())
                 {
-                    Minimum = minimum,
-                    Maximum = maximum,
-                    Value = 0
-                };
-                taskDialog.CommonButtons = TaskDialogCommonButtons.Ok;
+                    var minimum = 0;
+                    var maximum = 10000;
 
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link1", "Command Link1"));
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link2", "Command Link2"));
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link3", "Command Link3"));
-
-                taskDialog.Timer += (sender, args) =>
-                {
-                    var dialog = sender as TaskDialog;
-                    if (dialog != null)
+                    taskDialog.Cancelable = true;
+                    taskDialog.Title = "All Controls Sample";
+                    taskDialog.MainInstructionText = "Main Instruction Text.";
+                    taskDialog.ContentText = "Content Text.";
+                    taskDialog.FooterText = "Footer Text.";
+                    taskDialog.ExpandedText = "Expanded Text.";
+                    taskDialog.ExpandedControlText = "Expanded Label";
+                    taskDialog.CollapsedControlText = "Collapsed Label";
+                    taskDialog.VerificationText = "Verification Text.";
+                    taskDialog.MainIcon = TaskDialogIcon.Information;
+                    taskDialog.FooterIcon = TaskDialogIcon.Warning;
+                    taskDialog.ProgressBar = new TaskDialogProgressBar("progress")
                     {
-                        if (dialog.ProgressBar is TaskDialogProgressBar)
-                        {
-                            var progressBar = dialog.ProgressBar as TaskDialogProgressBar;
+                        Minimum = minimum,
+                        Maximum = maximum,
+                        Value = 0
+                    };
+                    taskDialog.CommonButtons = TaskDialogCommonButtons.Ok;
 
-                            if (args.Ticks <= maximum)
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link1", "Command Link1"));
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link2", "Command Link2"));
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link3", "Command Link3"));
+
+                    taskDialog.Timer += (sender, args) =>
+                    {
+                        var dialog = sender as TaskDialog;
+                        if (dialog != null)
+                        {
+                            if (dialog.ProgressBar is TaskDialogProgressBar)
                             {
-                                progressBar.Value = args.Ticks;
-                            }
-                            else
-                            {
-                                progressBar.Value = maximum;
+                                var progressBar = dialog.ProgressBar as TaskDialogProgressBar;
+
+                                if (args.Ticks <= maximum)
+                                {
+                                    progressBar.Value = args.Ticks;
+                                }
+                                else
+                                {
+                                    progressBar.Value = maximum;
+                                }
                             }
                         }
+                    };
+
+                    var result = taskDialog.Show();
+
+                    Debug.WriteLine("SelectedButton = " + result.SelectedButton);
+                    Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
+                    Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
+                }
+            });
+        }
+
+        public DispatcherOperation ShowCustomButtonTaskDialogAsync()
+        {
+            return this.Dispatcher.InvokeAsync(() =>
+            {
+                using (var taskDialog = new TaskDialog())
+                {
+                    taskDialog.Title = "Custom Button Sample";
+                    taskDialog.MainInstructionText = "Custom Buttons";
+                    taskDialog.Cancelable = true;
+
+                    taskDialog.CustomButtons.Add(new TaskDialogButton("button4", "Custom Button", true));
+                    taskDialog.CustomButtons.Add(new TaskDialogButton("button5", "Custom Button(Not Close Dialog)"));
+                    foreach (var button in taskDialog.CustomButtons)
+                    {
+                        button.Click += (sender, args) => Debug.WriteLine("Click: {0}", sender);
                     }
-                };
 
-                var result = taskDialog.Show();
+                    var result = taskDialog.Show();
 
-                Debug.WriteLine("SelectedButton = " + result.SelectedButton);
-                Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
-                Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
-            }
-        }
-
-        public void ShowCustomButtonTaskDialog()
-        {
-            using (var taskDialog = new TaskDialog())
-            {
-                taskDialog.Title = "Custom Button Sample";
-                taskDialog.MainInstructionText = "Custom Buttons";
-                taskDialog.Cancelable = true;
-
-                taskDialog.CustomButtons.Add(new TaskDialogButton("button4", "Custom Button", true));
-                taskDialog.CustomButtons.Add(new TaskDialogButton("button5", "Custom Button(Not Close Dialog)"));
-                foreach (var button in taskDialog.CustomButtons)
-                {
-                    button.Click += (sender, args) => Debug.WriteLine("Click: {0}", sender);
+                    Debug.WriteLine("SelectedButton = " + result.SelectedButton);
+                    Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
+                    Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
                 }
-
-                var result = taskDialog.Show();
-
-                Debug.WriteLine("SelectedButton = " + result.SelectedButton);
-                Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
-                Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
-            }
+            });
         }
 
-        public void ShowCommandLinkTaskDialog()
+        public DispatcherOperation ShowCommandLinkTaskDialogAsync()
         {
-            using (var taskDialog = new TaskDialog())
+            return this.Dispatcher.InvokeAsync(() =>
             {
-                taskDialog.Title = "Command Link Sample";
-                taskDialog.Cancelable = true;
-                taskDialog.CommonButtons = TaskDialogCommonButtons.Close;
-
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link1", "Custom Link1", true));
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link2", "Custom Link2", "Close Dialog", true));
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link3", "Custom Link3", "Close Dialog", true));
-                taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link4", "Custom Link4", "Not Close Dialog"));
-                foreach (var commandLink in taskDialog.CommandLinks)
+                using (var taskDialog = new TaskDialog())
                 {
-                    commandLink.Click += (sender, args) => Debug.WriteLine("Click: {0}", sender);
+                    taskDialog.Title = "Command Link Sample";
+                    taskDialog.Cancelable = true;
+                    taskDialog.CommonButtons = TaskDialogCommonButtons.Close;
+
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link1", "Custom Link1", true));
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link2", "Custom Link2", "Close Dialog", true));
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link3", "Custom Link3", "Close Dialog", true));
+                    taskDialog.CommandLinks.Add(new TaskDialogCommandLink("link4", "Custom Link4", "Not Close Dialog"));
+                    foreach (var commandLink in taskDialog.CommandLinks)
+                    {
+                        commandLink.Click += (sender, args) => Debug.WriteLine("Click: {0}", sender);
+                    }
+
+                    var result = taskDialog.Show();
+
+                    Debug.WriteLine("SelectedButton = " + result.SelectedButton);
+                    Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
+                    Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
                 }
-
-                var result = taskDialog.Show();
-
-                Debug.WriteLine("SelectedButton = " + result.SelectedButton);
-                Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
-                Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
-            }
+            });
         }
 
-        public void ShowRadioButtonTaskDialog()
+        public DispatcherOperation ShowRadioButtonTaskDialogAsync()
         {
-            using (var taskDialog = new TaskDialog())
+            return this.Dispatcher.InvokeAsync(() =>
             {
-                taskDialog.Title = "Radio Button Sample";
-                taskDialog.Cancelable = true;
-                taskDialog.CommonButtons = TaskDialogCommonButtons.Close;
+                using (var taskDialog = new TaskDialog())
+                {
+                    taskDialog.Title = "Radio Button Sample";
+                    taskDialog.Cancelable = true;
+                    taskDialog.CommonButtons = TaskDialogCommonButtons.Close;
 
-                taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio1", "Radio Button1") { Default = true });
-                taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio2", "Radio Button2"));
-                taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio3", "Radio Button3"));
-                taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio4", "Radio Button4"));
-                taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio5", "Radio Button5"));
+                    taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio1", "Radio Button1") {Default = true});
+                    taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio2", "Radio Button2"));
+                    taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio3", "Radio Button3"));
+                    taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio4", "Radio Button4"));
+                    taskDialog.RadioButtons.Add(new TaskDialogRadioButton("radio5", "Radio Button5"));
 
-                var result = taskDialog.Show();
+                    var result = taskDialog.Show();
 
-                Debug.WriteLine("SelectedButton = " + result.SelectedButton);
-                Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
-                Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
-            }
+                    Debug.WriteLine("SelectedButton = " + result.SelectedButton);
+                    Debug.WriteLine("SelectedRadioButton = " + result.SelectedRadioButton);
+                    Debug.WriteLine("VerificationChecked = " + result.VerificationChecked);
+                }
+            });
         }
 
-        public void ShowMarqueeTaskDialog()
+        public DispatcherOperation ShowMarqueeTaskDialogAsync()
         {
-            using (var taskDialog = new TaskDialog())
+            return  this.Dispatcher.InvokeAsync(() =>
             {
-                taskDialog.Title = "Marquee Sample";
-                taskDialog.Cancelable = true;
-                taskDialog.ProgressBar = new TaskDialogMarquee("Marquee");
+                using (var taskDialog = new TaskDialog())
+                {
+                    taskDialog.Title = "Marquee Sample";
+                    taskDialog.Cancelable = true;
+                    taskDialog.ProgressBar = new TaskDialogMarquee("Marquee");
 
-                taskDialog.Show();
-            }
+                    taskDialog.Show();
+                }
+            });
         }
     }
 }
