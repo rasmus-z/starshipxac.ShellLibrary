81c478f5208413df6dd61d87a270320f85e28bcf
starshipxac <starshipxac@users.noreply.github.com>
2017-02-07 23:05:10 +0900

    Modify comment.

--- Source/starshipxac.Shell/ShellException.cs
+++ Source/starshipxac.Shell/ShellException.cs
@@ -7,63 +7,67 @@ using starshipxac.Shell.Interop;
 namespace starshipxac.Shell
 {
     /// <summary>
-    ///     <c>Shell</c>例外クラスを定義します。
+    ///     Define <c>Shell</c> exception class.
     /// </summary>
     [Serializable]
     public class ShellException : ExternalException
     {
         /// <summary>
-        ///     <see cref="ShellException" />クラスの新しいインスタンスを初期化します。
+        ///     Initialize a new instance of the <see cref="ShellException" /> class.
         /// </summary>
         public ShellException()
         {
         }
 
         /// <summary>
-        ///     メッセージを指定して、<see cref="ShellException" />クラスの新しいインスタンスを初期化します。
+        ///     Initialize a new instance of the <see cref="ShellException" /> class
+        ///     to the specified message.
         /// </summary>
-        /// <param name="message">メッセージ。</param>
+        /// <param name="message">Message.</param>
         public ShellException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        ///     メッセージと<see cref="Exception" />を指定して、<see cref="ShellException" />クラスの新しいインスタンスを初期化します。
+        ///     Initialize a new instance of the <see cref="ShellException" /> class
+        ///     to the specified message and internal <see cref="Exception"/>.
         /// </summary>
-        /// <param name="message">メッセージ。</param>
-        /// <param name="innerException">以前に発生した<see cref="Exception" />。</param>
+        /// <param name="message">Message.</param>
+        /// <param name="innerException"><see cref="Exception"/> that occurred before.</param>
         public ShellException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
         /// <summary>
-        ///     メッセージとエラーコードを指定して、<see cref="ShellException" />クラスの新しいインスタンスを初期化します。
+        ///     Initialize a new instance of the <see cref="ShellException" /> class
+        ///     to the specified message and error code.
         /// </summary>
-        /// <param name="message">メッセージ。</param>
-        /// <param name="errorCode">エラーコード。</param>
+        /// <param name="message">Message.</param>
+        /// <param name="errorCode">Error code.</param>
         public ShellException(string message, int errorCode)
             : base(message, errorCode)
         {
         }
 
         /// <summary>
-        ///     メッセージと<see cref="HRESULT" />を指定して、<see cref="ShellException" />クラスの新しいインスタンスを初期化します。
+        ///     Initialize a new instance of the <see cref="ShellException" /> class
+        ///     to the specified message and <see cref="HRESULT"/>.
         /// </summary>
-        /// <param name="message">メッセージ。</param>
-        /// <param name="errorCode"><see cref="HRESULT" />エラーコード。</param>
+        /// <param name="message">Message.</param>
+        /// <param name="errorCode"><see cref="HRESULT" /> Error code.</param>
         internal ShellException(string message, HRESULT errorCode)
             : this(message, (int)errorCode)
         {
         }
 
         /// <summary>
-        ///     シリアル化したデータを指定して、
-        ///     <see cref="ShellException" />クラスの新しいインスタンスを初期化します。
+        ///     Initialize a new instance of the <see cref="ShellException" /> class
+        ///     to the specified serialize data.
         /// </summary>
-        /// <param name="info"><see cref="SerializationInfo" />。</param>
-        /// <param name="context"><see cref="StreamingContext" />。</param>
+        /// <param name="info"><see cref="SerializationInfo" />.</param>
+        /// <param name="context"><see cref="StreamingContext" />.</param>
         protected ShellException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
