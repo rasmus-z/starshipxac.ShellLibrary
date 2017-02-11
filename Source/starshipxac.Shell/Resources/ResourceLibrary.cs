c0994592efe5edc4cdfbab510fae8a7a38c46b53
starshipxac <starshipxac@users.noreply.github.com>
2017-02-12 00:12:41 +0900

    Remove comment.

--- Source/starshipxac.Shell/Resources/ResourceLibrary.cs
+++ Source/starshipxac.Shell/Resources/ResourceLibrary.cs
@@ -93,51 +93,6 @@ namespace starshipxac.Shell.Resources
             return this.instanceHandle;
         }
 
-        //#region Native Methods
-        ////TODO: Unmanagedの分離
-        //private const UInt32 DONT_RESOLVE_DLL_REFERENCES = 0x00000001;
-        //private const UInt32 LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010;
-        //private const UInt32 LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
-        //private const UInt32 LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040;
-        //private const UInt32 LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020;
-        //private const UInt32 LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008;
-
-        //private const UInt32 IMAGE_BITMAP = 0;
-        //private const UInt32 IMAGE_ICON = 1;
-        //private const UInt32 IMAGE_CURSOR = 2;
-
-        //private const UInt32 LR_CREATEDIBSECTION = 0x00002000;
-        //private const UInt32 LR_DEFAULTCOLOR = 0x00000000;
-        //private const UInt32 LR_DEFAULTSIZE = 0x00000040;
-        //private const UInt32 LR_LOADFROMFILE = 0x00000010;
-        //private const UInt32 LR_LOADMAP3DCOLORS = 0x00001000;
-        //private const UInt32 LR_LOADTRANSPARENT = 0x00000020;
-        //private const UInt32 LR_MONOCHROME = 0x00000001;
-        //private const UInt32 LR_SHARED = 0x00008000;
-        //private const UInt32 LR_VGACOLOR = 0x00000080;
-
-        //[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        //private static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, UInt32 dwFlags);
-
-        //private static IntPtr LoadLibraryEx(string lpFileName, UInt32 dwFlags)
-        //{
-        //    return LoadLibraryEx(lpFileName, IntPtr.Zero, dwFlags);
-        //}
-
-        //[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        //private static extern int LoadString(IntPtr instanceHandle, int id, StringBuilder buffer, int bufferSize);
-
-        //[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        //private static extern IntPtr LoadImage(IntPtr hInstance, string lpszName, UInt32 uType,
-        //    int cxDesired, int cyDesired, UInt32 fuLoad);
-
-        //private static IntPtr LoadIconImage(IntPtr hInstance, string lpszName, int cxDesired, int cyDesired, uint fuLoad)
-        //{
-        //    return LoadImage(hInstance, lpszName, IMAGE_ICON, cxDesired, cyDesired, fuLoad);
-        //}
-
-        //#endregion
-
         public override string ToString()
         {
             return $"{{ LibraryPath: {LibraryPath} }}";
