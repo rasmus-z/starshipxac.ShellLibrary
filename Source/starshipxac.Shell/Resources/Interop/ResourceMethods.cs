6605ee5771332370864c56590b9b0c2377ce3050
starshipxac <starshipxac@users.noreply.github.com>
2017-02-12 00:12:05 +0900

    Modify ResourceLibrary class.

---
+++ Source/starshipxac.Shell/Resources/Interop/ResourceMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace starshipxac.Shell.Resources.Interop
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    internal static class ResourceMethods
+    {
+        internal const UInt32 DONT_RESOLVE_DLL_REFERENCES = 0x00000001;
+        internal const UInt32 LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010;
+        internal const UInt32 LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
+        internal const UInt32 LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040;
+        internal const UInt32 LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020;
+        internal const UInt32 LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008;
+
+        internal const UInt32 IMAGE_BITMAP = 0;
+        internal const UInt32 IMAGE_ICON = 1;
+        internal const UInt32 IMAGE_CURSOR = 2;
+
+        internal const UInt32 LR_CREATEDIBSECTION = 0x00002000;
+        internal const UInt32 LR_DEFAULTCOLOR = 0x00000000;
+        internal const UInt32 LR_DEFAULTSIZE = 0x00000040;
+        internal const UInt32 LR_LOADFROMFILE = 0x00000010;
+        internal const UInt32 LR_LOADMAP3DCOLORS = 0x00001000;
+        internal const UInt32 LR_LOADTRANSPARENT = 0x00000020;
+        internal const UInt32 LR_MONOCHROME = 0x00000001;
+        internal const UInt32 LR_SHARED = 0x00008000;
+        internal const UInt32 LR_VGACOLOR = 0x00000080;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, UInt32 dwFlags);
+
+        internal static IntPtr LoadLibraryEx(string lpFileName, UInt32 dwFlags)
+        {
+            return LoadLibraryEx(lpFileName, IntPtr.Zero, dwFlags);
+        }
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern int LoadString(IntPtr instanceHandle, int id, StringBuilder buffer, int bufferSize);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr LoadImage(IntPtr hInstance, string lpszName, UInt32 uType,
+            int cxDesired, int cyDesired, UInt32 fuLoad);
+
+        internal static IntPtr LoadIconImage(IntPtr hInstance, string lpszName, int cxDesired, int cyDesired, uint fuLoad)
+        {
+            return LoadImage(hInstance, lpszName, IMAGE_ICON, cxDesired, cyDesired, fuLoad);
+        }
+    }
+}
--- Source/starshipxac.Shell/Resources/ResourceLibrary.cs
+++ Source/starshipxac.Shell/Resources/ResourceLibrary.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics.Contracts;
-using System.Runtime.InteropServices;
 using System.Text;
 using starshipxac.Shell.Media.Imaging;
+using starshipxac.Shell.Resources.Interop;
 
 namespace starshipxac.Shell.Resources
 {
@@ -15,7 +15,7 @@ namespace starshipxac.Shell.Resources
         private IntPtr instanceHandle = IntPtr.Zero;
 
         /// <summary>
-        ///     Initialize a new instance of the <see cref="ResourceLibrary"/> class
+        ///     Initialize a new instance of the <see cref="ResourceLibrary" /> class
         ///     to the specified resource DLL path.
         /// </summary>
         /// <param name="libraryPath">Resource DLL path.</param>
@@ -44,7 +44,7 @@ namespace starshipxac.Shell.Resources
             var hInstance = GetInstanceHandle();
 
             var result = new StringBuilder(255);
-            LoadString(hInstance, resourceId, result, result.Capacity);
+            ResourceMethods.LoadString(hInstance, resourceId, result, result.Capacity);
 
             return result.ToString();
         }
@@ -67,7 +67,7 @@ namespace starshipxac.Shell.Resources
             var hInstance = GetInstanceHandle();
 
             var resourceName = $"#{resourceId}";
-            var hicon = LoadIconImage(hInstance, resourceName, size, size, LR_DEFAULTCOLOR);
+            var hicon = ResourceMethods.LoadIconImage(hInstance, resourceName, size, size, ResourceMethods.LR_DEFAULTCOLOR);
             if (hicon == IntPtr.Zero)
             {
                 throw new Win32Exception();
@@ -84,7 +84,7 @@ namespace starshipxac.Shell.Resources
         {
             if (this.instanceHandle == IntPtr.Zero)
             {
-                this.instanceHandle = LoadLibraryEx(this.LibraryPath, LOAD_LIBRARY_AS_DATAFILE);
+                this.instanceHandle = ResourceMethods.LoadLibraryEx(this.LibraryPath, ResourceMethods.LOAD_LIBRARY_AS_DATAFILE);
                 if (this.instanceHandle == IntPtr.Zero)
                 {
                     throw new Win32Exception();
@@ -93,50 +93,50 @@ namespace starshipxac.Shell.Resources
             return this.instanceHandle;
         }
 
-        #region Native Methods
-        //TODO: Unmanagedの分離
-        private const UInt32 DONT_RESOLVE_DLL_REFERENCES = 0x00000001;
-        private const UInt32 LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010;
-        private const UInt32 LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
-        private const UInt32 LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040;
-        private const UInt32 LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020;
-        private const UInt32 LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008;
+        //#region Native Methods
+        ////TODO: Unmanagedの分離
+        //private const UInt32 DONT_RESOLVE_DLL_REFERENCES = 0x00000001;
+        //private const UInt32 LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010;
+        //private const UInt32 LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
+        //private const UInt32 LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040;
+        //private const UInt32 LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020;
+        //private const UInt32 LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008;
 
-        private const UInt32 IMAGE_BITMAP = 0;
-        private const UInt32 IMAGE_ICON = 1;
-        private const UInt32 IMAGE_CURSOR = 2;
+        //private const UInt32 IMAGE_BITMAP = 0;
+        //private const UInt32 IMAGE_ICON = 1;
+        //private const UInt32 IMAGE_CURSOR = 2;
 
-        private const UInt32 LR_CREATEDIBSECTION = 0x00002000;
-        private const UInt32 LR_DEFAULTCOLOR = 0x00000000;
-        private const UInt32 LR_DEFAULTSIZE = 0x00000040;
-        private const UInt32 LR_LOADFROMFILE = 0x00000010;
-        private const UInt32 LR_LOADMAP3DCOLORS = 0x00001000;
-        private const UInt32 LR_LOADTRANSPARENT = 0x00000020;
-        private const UInt32 LR_MONOCHROME = 0x00000001;
-        private const UInt32 LR_SHARED = 0x00008000;
-        private const UInt32 LR_VGACOLOR = 0x00000080;
+        //private const UInt32 LR_CREATEDIBSECTION = 0x00002000;
+        //private const UInt32 LR_DEFAULTCOLOR = 0x00000000;
+        //private const UInt32 LR_DEFAULTSIZE = 0x00000040;
+        //private const UInt32 LR_LOADFROMFILE = 0x00000010;
+        //private const UInt32 LR_LOADMAP3DCOLORS = 0x00001000;
+        //private const UInt32 LR_LOADTRANSPARENT = 0x00000020;
+        //private const UInt32 LR_MONOCHROME = 0x00000001;
+        //private const UInt32 LR_SHARED = 0x00008000;
+        //private const UInt32 LR_VGACOLOR = 0x00000080;
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        private static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, UInt32 dwFlags);
+        //[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        //private static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, UInt32 dwFlags);
 
-        private static IntPtr LoadLibraryEx(string lpFileName, UInt32 dwFlags)
-        {
-            return LoadLibraryEx(lpFileName, IntPtr.Zero, dwFlags);
-        }
+        //private static IntPtr LoadLibraryEx(string lpFileName, UInt32 dwFlags)
+        //{
+        //    return LoadLibraryEx(lpFileName, IntPtr.Zero, dwFlags);
+        //}
 
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        private static extern int LoadString(IntPtr instanceHandle, int id, StringBuilder buffer, int bufferSize);
+        //[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        //private static extern int LoadString(IntPtr instanceHandle, int id, StringBuilder buffer, int bufferSize);
 
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        private static extern IntPtr LoadImage(IntPtr hInstance, string lpszName, UInt32 uType,
-            int cxDesired, int cyDesired, UInt32 fuLoad);
+        //[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        //private static extern IntPtr LoadImage(IntPtr hInstance, string lpszName, UInt32 uType,
+        //    int cxDesired, int cyDesired, UInt32 fuLoad);
 
-        private static IntPtr LoadIconImage(IntPtr hInstance, string lpszName, int cxDesired, int cyDesired, uint fuLoad)
-        {
-            return LoadImage(hInstance, lpszName, IMAGE_ICON, cxDesired, cyDesired, fuLoad);
-        }
+        //private static IntPtr LoadIconImage(IntPtr hInstance, string lpszName, int cxDesired, int cyDesired, uint fuLoad)
+        //{
+        //    return LoadImage(hInstance, lpszName, IMAGE_ICON, cxDesired, cyDesired, fuLoad);
+        //}
 
-        #endregion
+        //#endregion
 
         public override string ToString()
         {
